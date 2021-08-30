63a5dc655ae9ce7d49d2bb34dd9628d96d932f36
Damian Duffy <d@damianduffy.com>
2021-08-30 22:46:56 +0100

    displaying player character

--- game.py
+++ game.py
@@ -2,8 +2,37 @@
 import tcod
 
 
-def main():
-    print("Hello World!!")
+def main() -> None:
+    # --- config settings ---
+    screen_width = 80
+    screen_height = 50
+    player_x = int(screen_width / 2)
+    player_y = int(screen_height / 2)
+    tileset = tcod.tileset.load_tilesheet(
+        "data/img/dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
+    )
+    
+    # --- generate the console ---
+    with tcod.context.new_terminal(
+        screen_width, 
+        screen_height, 
+        tileset = tileset,
+        title = "My First Roguelike",
+        vsync = True,
+    ) as context:
+        root_console = tcod.Console(screen_width, screen_height, order="F")
+        # --- main game loop ---
+        while True:
+            # --- draw to screen ---
+            root_console.print(x=player_x, y=player_y, string="@")
+            
+            # --- update the screen ---
+            context.present(root_console)
+
+            # --- capture input ---
+            for event in tcod.event.wait():
+                if event.type == "QUIT":
+                    raise SystemExit()
 
 
 if __name__ == "__main__":
